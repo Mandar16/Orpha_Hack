f1280d68fee3e23912a909ffc3b83536bfea535e
Mandar16 <iit2019059@iiita.ac.in>
2021-03-03 13:35:43 -0800

    Added activity_main.xml

---
+++ app/src/main/java/com/example/orpha/OrphanageRegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.example.orpha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OrphanageRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_orphanage_register)
+    }
+}
